a2948df3c066e0e7cb0f72074efc33a04b529124
Christoph Quergfelder <quergfelderch92068@th-nuernberg.de>
2025-05-29 15:56:22 +0200

    create save main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,36 @@
 #include <iostream>
 #include <cstring>
 
+const int = 10;
+
 void vulnerableFunction(char* input) {
-    char buffer[10];
+    char buffer[BUFFER_SIZE];
     strcpy(buffer, input);
     std::cout << "Input was: " << buffer << std::endl;
 }
 
 int main() {
-
-    // Declare the variables
     char* maybeLargeInput;
 
-    // Read input
     std::cout << "Enter your name. Maximum 9 digits: ";
+    std::cin.width(BUFFER_SIZE);
     std::cin >> maybeLargeInput;
 
-    vulnerableFunction(maybeLargeInput);
+    if(maybeLargeInput == nullptr) {
+        std::cerr << "Error: Input is null." << std::endl;
+        return 1;
+    }
+    else if(strlen(maybeLargeInput) >= BUFFER_SIZE) {
+        std::cerr << "Error: Input exceeds buffer size." << std::endl;
+        return 1;
+    }
+    else
+    {
+        vulnerableFunction(maybeLargeInput);
+    }
+    std::cout << "Program completed successfully." << std::endl;
+
+    delete[] maybeLargeInput;
+
     return 0;
 }
